41688cc12cb124611f2083e556ca3c1c461048a8
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2019-05-07 16:46:25 +0300

    добавляет функцию обработки даты

--- function.php
+++ function.php
@@ -30,3 +30,23 @@ function include_template($name, array $data = []) {
 
     return $result;
 }
+
+/**
+ * Функция подсчитывает количество часов до даты указаной в параметре
+ * @param date -заданая дата
+ * @return int количество часов до даты в параметре
+ */
+function get_hour_date($date) {
+    date_default_timezone_set('Europe/Kiev');
+    $str = strtotime($date);
+    $t = time();
+    if($str>$t){
+        $k = $str-$t;
+        return floor($k/3600);
+        }
+    return 999;
+
+}
+
+//echo get_hour_date('07.05.2019');
+echo get_hour_date('08.05.2019');
